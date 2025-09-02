8d982901f477b7ed477c2c7595e93c8cbe9d63c9
Joseph King <joking164@icloud.com>
2025-09-02 14:36:34 -0400

    3025 go, py, js

---
+++ 3025/solution.js
@@ -0,0 +1,39 @@
+let numberOfPairs = function(points){
+    let pairs = 0
+
+    for(let i in points){
+        for(let j in points){
+            if(i === j){
+                continue
+            }
+
+            if(points[i][0] >= points[j][0] && points[i][1] <= points[j][1] && isNotWithinPair(points, i, j)){
+                pairs++
+            }
+        }
+    }
+
+    return pairs
+}
+
+let isNotWithinPair = function(points, i, j){
+    for(let x in points){
+        if(x === i || x === j){
+            continue
+        }
+
+        if(points[x][0] <= points[i][0] && points[x][0] >= points[j][0] && points[x][1] >= points[i][1] && points[x][1] <= points[j][1]){
+            return false
+        }
+    }
+
+    return true
+}
+
+var case1 = [ [1,1], [2,2], [3,3] ]
+var case2 = [ [6,2], [4,4], [2,6] ]
+var case3 = [ [3,1], [1,3], [1,1] ]
+
+console.log(numberOfPairs(case1))
+console.log(numberOfPairs(case2))
+console.log(numberOfPairs(case3))
